f76228adab539d3736fa6b09b9629c8d9e8ae8a9
Elcand <tjandraraharja99@gmail.com>
2025-04-10 15:11:05 +0700

    Session - Storing data at session

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('session', function (Request $request) {
+    // $request->session()->put('foo', ['php', 'laravel', 'vue']);
+    session(['test' => 'bar']);
+    return view('session');
 });
