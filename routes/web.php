0cf0387b46475fa07d4c80da466b727d89211812
Elcand <tjandraraharja99@gmail.com>
2025-04-11 11:01:33 +0700

    Session - Deleting data from session

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('session', function (Request $request) {
-    // $request->session()->put('foo', ['php', 'laravel', 'vue']);
-    // session(['cart' => ['product 1', 'product 2', 'product 3']]);
+    // $request->session()->put('foo', ['php', 'laravel', 'vue']);`
+    // session(['cart' => ['product 1', 'product 2', 'product 3']]);`
     // $value = session('testFast', 'this is default value');
-    $value = session('test');
-    dd($value);
+    // $value = session('test');
+    // dd($value);
+
+    Session::forget(['cart', 'foo', 'test']);
 
     return view('session',);
 });
