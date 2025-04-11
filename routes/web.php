61d99c9bd1605fd953d28e8a715a7fadded82351
Elcand <tjandraraharja99@gmail.com>
2025-04-11 11:50:51 +0700

    Storing Data In Cache

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('cache', function () {
+    Cache::put('post', 'post  title one', $second = 5);
+    $value =  Cache::get('post');
+
+    dd($value);
 });
